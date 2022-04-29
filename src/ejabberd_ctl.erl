chrisatta/mongoose

Add ejabberd_sm_cets
Use cets for stream resumption backend
Print cets tables in mongoosectl  mnesia info
Enable cets on CI
Add test for mongooseimctl mnesia info
Test ejabberd_sm_cets in ejabberd_sm_SUITE
Rename to cets

src/ejabberd_ctl.erl
@@ -170,6 +170,7 @@ process(["mnesia"]) ->
     ?STATUS_SUCCESS;
 process(["mnesia", "info"]) ->
     mnesia:info(),
+    cets_info(),
     ?STATUS_SUCCESS;
 process(["mnesia", Arg]) when is_list(Arg) ->
     case catch mnesia:system_info(list_to_atom(Arg)) of
@@ -928,3 +929,15 @@ get_dist_proto() ->
         _ -> "inet_tcp"
     end.
 
+cets_info() ->
+    Tables = cets_discovery:info(mongoose_cets_discovery),
+    cets_info(Tables).
+
+cets_info([]) ->
+    ok;
+cets_info(Tables) ->
+    ?PRINT("Kiss tables:~n", []),
+    [cets_table_info(Table) || Table <- Tables].
+
+cets_table_info(#{memory := Memory, size := Size, nodes := Nodes, table := Tab}) ->
+    ?PRINT("table=~0p size=~p memory_words=~0p nodes=~0p~n", [Tab, Size, Memory, Nodes]).

apontini/MongooseIM

src/ejabberd_ctl.erl
@@ -170,6 +170,7 @@ process(["mnesia"]) ->
     ?STATUS_SUCCESS;
 process(["mnesia", "info"]) ->
     mnesia:info(),
+    cets_info(),
     ?STATUS_SUCCESS;
 process(["mnesia", Arg]) when is_list(Arg) ->
     case catch mnesia:system_info(list_to_atom(Arg)) of
@@ -928,3 +929,15 @@ get_dist_proto() ->
         _ -> "inet_tcp"
     end.
 
+cets_info() ->
+    Tables = cets_discovery:info(mongoose_cets_discovery),
+    cets_info(Tables).
+
+cets_info([]) ->
+    ok;
+cets_info(Tables) ->
+    ?PRINT("CETS tables:~n", []),
+    [cets_table_info(Table) || Table <- Tables].
+
+cets_table_info(#{memory := Memory, size := Size, nodes := Nodes, table := Tab}) ->
+    ?PRINT("table=~0p size=~p memory_words=~0p nodes=~0p~n", [Tab, Size, Memory, Nodes]).