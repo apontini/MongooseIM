283a5b14ddc56ec208e7d2a8979f18b83f48c7ce
Paweł Chrząszcz <pawel.chrzaszcz@erlang-solutions.com>
2022-06-21 09:28:34 +0200

    WIP

--- src/mongoose_client_api/mongoose_client_api_contacts.erl
+++ src/mongoose_client_api/mongoose_client_api_contacts.erl
@@ -162,13 +162,11 @@ get_whole_body(Req, Acc) ->
     end.
 
 handle_request(<<"GET">>, undefined, undefined, CJid, _State) ->
-    mongoose_commands:execute(CJid, list_contacts, #{caller => CJid});
+    list_contacts(CJid);
 handle_request(<<"POST">>, Jid, undefined, CJid, _State) ->
-    mongoose_commands:execute(CJid, add_contact, #{caller => CJid,
-        jid => Jid});
+    add_contact(CJid, Jid);
 handle_request(<<"DELETE">>, Jids, _Action, CJid, _State) when is_list(Jids) ->
-    mongoose_commands:execute(CJid, delete_contacts, #{caller => CJid,
-        jids => Jids});
+    delete_contacts(CJid, Jids);
 handle_request(Method, Jid, Action, CJid, #{jid := CallerJid, creds := Creds}) ->
     HostType = mongoose_credentials:host_type(Creds),
     case contact_exists(HostType, CallerJid, jid:from_binary(Jid)) of
@@ -178,14 +176,11 @@ handle_request(Method, Jid, Action, CJid, #{jid := CallerJid, creds := Creds}) -
     end.
 
 handle_contact_request(<<"PUT">>, Jid, <<"invite">>, CJid) ->
-    mongoose_commands:execute(CJid, subscription, #{caller => CJid,
-        jid => Jid, action => atom_to_binary(subscribe, latin1)});
+    subscription(CJid, Jid, atom_to_binary(subscribe, latin1));
 handle_contact_request(<<"PUT">>, Jid, <<"accept">>, CJid) ->
-    mongoose_commands:execute(CJid, subscription, #{caller => CJid,
-        jid => Jid, action => atom_to_binary(subscribed, latin1)});
+    subscription(CJid, Jid, atom_to_binary(subscribed, latin1));
 handle_contact_request(<<"DELETE">>, Jid, undefined, CJid) ->
-    mongoose_commands:execute(CJid, delete_contact, #{caller => CJid,
-        jid => Jid});
+    delete_contact(CJid, Jid);
 handle_contact_request(_, _, _, _) ->
     not_implemented.
 
@@ -200,3 +195,72 @@ contact_exists(HostType, CallerJid, Jid) ->
     LJid = jid:to_lower(Jid),
     Res = mod_roster:get_roster_entry(HostType, CallerJid, LJid, short),
     Res =/= does_not_exist andalso Res =/= error.
+
+%% Internal functions
+
+list_contacts(Caller) ->
+    case mod_roster_api:list_contacts(jid:from_binary(Caller)) of
+        {ok, Rosters} ->
+            {ok, [roster_info(mod_roster:item_to_map(R)) || R <- Rosters]};
+        Error ->
+            skip_result_msg(Error)
+    end.
+
+roster_info(M) ->
+    Jid = jid:to_binary(maps:get(jid, M)),
+    #{subscription := Sub, ask := Ask} = M,
+    #{jid => Jid, subscription => Sub, ask => Ask}.
+
+skip_result_msg({ok, _Msg}) -> ok;
+skip_result_msg({ErrCode, _Msg}) -> {error, ErrCode}.
+
+add_contact(Caller, JabberID) ->
+    add_contact(Caller, JabberID, <<>>, []).
+
+add_contact(Caller, Other, Name, Groups) ->
+    case mongoose_stanza_helper:parse_from_to(Caller, Other) of
+        {ok, CallerJid, OtherJid} ->
+            Res = mod_roster_api:add_contact(CallerJid, OtherJid, Name, Groups),
+            skip_result_msg(Res);
+        E ->
+            E
+    end.
+
+delete_contacts(Caller, ToDelete) ->
+    maybe_delete_contacts(Caller, ToDelete, []).
+
+maybe_delete_contacts(_, [], NotDeleted) -> NotDeleted;
+maybe_delete_contacts(Caller, [H | T], NotDeleted) ->
+    case delete_contact(Caller, H) of
+        ok ->
+            maybe_delete_contacts(Caller, T, NotDeleted);
+        _Error ->
+            maybe_delete_contacts(Caller, T, NotDeleted ++ [H])
+    end.
+
+delete_contact(Caller, Other) ->
+    case mongoose_stanza_helper:parse_from_to(Caller, Other) of
+        {ok, CallerJID, OtherJID} ->
+            Res = mod_roster_api:delete_contact(CallerJID, OtherJID),
+            skip_result_msg(Res);
+        E ->
+            E
+    end.
+
+subscription(Caller, Other, Action) ->
+    case decode_action(Action) of
+        error ->
+            {error, bad_request, <<"invalid action">>};
+        Act ->
+            case mongoose_stanza_helper:parse_from_to(Caller, Other) of
+                {ok, CallerJID, OtherJID} ->
+                    Res = mod_roster_api:subscription(CallerJID, OtherJID, Act),
+                    skip_result_msg(Res);
+                E ->
+                    E
+            end
+    end.
+
+decode_action(<<"subscribe">>) -> subscribe;
+decode_action(<<"subscribed">>) -> subscribed;
+decode_action(_) -> error.
